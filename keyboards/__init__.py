7ec0ea73cdcdf342aaa50403ad9af3a518810cfb
elenilda10 <126091743+elenilda10@users.noreply.github.com>
2026-08-25 20:17:21 -0300

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,5 +1,6 @@
 from . import inline_keyboards as _inline_keyboards
 
+# Exporta todas as funções e teclados, ignorando apenas membros internos/privados
 __all__ = [name for name in dir(_inline_keyboards) if not name.startswith("_")]
 
 globals().update({name: getattr(_inline_keyboards, name) for name in __all__})
